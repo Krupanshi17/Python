6af536a7c7444046b05b768a7e9e9cc8bec1b86d
Krupanshi17 <gandhikrupanshi@gmail.com>
2026-01-20 11:54:52 +0530

    Inroduction of the Python

---
+++ introduction.py
@@ -0,0 +1,13 @@
+"""
+PART 1 – PYTHON FUNDAMENTALS
+TOPIC 1: INTRODUCTION & SYNTAX
+
+This file covers Python basics used in real-world projects.
+"""
+
+# First Python program
+print("Hello, Python!")
+
+# Demonstrating indentation
+if True:
+    print("Python uses indentation instead of braces")
