4918a0b534399409b0092029c8e252ceba1214b8
Krupanshi17 <gandhikrupanshi@gmail.com>
2026-01-28 10:03:14 +0530

    Http Request

--- 23.Requests.py
+++ 23.Requests.py
@@ -6,4 +6,16 @@ response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 print(response)
 print(response.text)
 
+#Example of https methods
 
+payload={
+    'title':'Learning RestAPI',
+    'body':'This is a test post',
+    'id':1
+}
+
+response_post=requests.post("https://jsonplaceholder.typicode.com/posts",
+                            json=payload)
+
+print(response_post.status_code)
+print(response_post.json)
