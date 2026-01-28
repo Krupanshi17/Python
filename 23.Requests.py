ba9e26a6b0c4b6e680ae878a94ca9733dffbf88c
Krupanshi17 <gandhikrupanshi@gmail.com>
2026-01-28 10:13:59 +0530

    Http Requests

--- 23.Requests.py
+++ 23.Requests.py
@@ -19,3 +19,16 @@ response_post=requests.post("https://jsonplaceholder.typicode.com/posts",
 
 print(response_post.status_code)
 print(response_post.json)
+
+update={
+    'title':'Updated Title'
+}
+
+response_put=requests.put("https://jsonplaceholder.typicode.com/posts/1",json=update)
+
+print(response_put.status_code)
+print(response_put.json)
+
+responce_delete=requests.delete("https://jsonplaceholder.typicode.com/posts/1")
+
+print(responce_delete.status_code)
