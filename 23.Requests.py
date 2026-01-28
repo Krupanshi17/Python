16b0cfd6e40a14c6504170d3a22f6443ca481539
Krupanshi17 <gandhikrupanshi@gmail.com>
2026-01-28 10:17:21 +0530

    Http Method

--- 23.Requests.py
+++ 23.Requests.py
@@ -1,3 +1,18 @@
+"""HTTP stands for HyperText Transfer Protocol. It is the foundation of any data exchange on the 
+Web and it is a protocol used for transmitting hypertext requests and information on the internet.
+HTTP follows a classical client-server model where the client sends a request to the server, and 
+the server responds with the requested resource or an error message.
+In Python, the `requests` library is a popular choice for making HTTP requests. It provides a 
+simple and intuitive API for sending HTTP requests and handling responses."""
+
+#Get-to fetch data from a specified resource
+#Post-to submit data to be processed to a specified resource
+#Put-to update a current resource with new data
+#Delete-to delete a specified resource
+
+
+
+
 import requests
 
 
